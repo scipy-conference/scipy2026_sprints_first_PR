322be18ef744b237e3fc1243767454454236cf79
dkl <>
2026-07-17 19:24:36 -0400

    error on negative

--- participants/scipy_052/pr_tutorial/tests/test_simple_function.py
+++ participants/scipy_052/pr_tutorial/tests/test_simple_function.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pr_tutorial.simple_functions import factorial
 
 
@@ -8,5 +10,17 @@ def test_factorial_3():
 
 
 def test_factorial_0():
-    """Check base case"""
+    """Test base case"""
     assert factorial(0) == 1
+
+
+def test_factorial_non_neg():
+    """Error on negative value"""
+    with pytest.raises(RuntimeError, match="negative"):
+        factorial(-1)
+
+
+def test_factorial_float():
+    """Error (eventually) for float being passed"""
+    with pytest.raises(RuntimeError, match="negative"):
+        factorial(1.5)
