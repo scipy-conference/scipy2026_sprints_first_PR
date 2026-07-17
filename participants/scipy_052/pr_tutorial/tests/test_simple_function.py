93e4fabc88d035c253e79e4779f94f94cbc70f10
dkl <>
2026-07-17 19:17:16 -0400

    added test for base case

--- participants/scipy_052/pr_tutorial/tests/test_simple_function.py
+++ participants/scipy_052/pr_tutorial/tests/test_simple_function.py
@@ -5,3 +5,8 @@ def test_factorial_3():
     """Simplest test for one crete case"""
 
     assert factorial(3) == 6
+
+
+def test_factorial_0():
+    """Check base case"""
+    assert factorial(0) == 1
